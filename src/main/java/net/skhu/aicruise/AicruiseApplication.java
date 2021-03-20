49b82b4c4520b55bcbb1192174fd69ab33118c1a
iris9602@naver.com <alstjs3795>
2021-03-21 01:55:11 +0900

    Initial commit

---
+++ src/main/java/net/skhu/aicruise/AicruiseApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.aicruise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicruiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicruiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/skhu/aicruise/AicruiseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu.aicruise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicruiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
