ff9e8d20ba4b5b167b3dede06405800d7652fe52
iris9602@naver.com <alstjs3795>
2021-03-21 02:12:49 +0900

    CHORE proxy server setting

---
+++ src/main/java/net/skhu/aicruise/HomeController.java
@@ -0,0 +1,12 @@
+package net.skhu.aicruise;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello";
+    }
+}
